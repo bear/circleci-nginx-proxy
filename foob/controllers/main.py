ff37b255b5432ace8cf1335f50c1cd53d21510c2
bear (Mike Taylor) <bear@bear.im>
2016-07-12 19:07:20 -0400

    lint cleanup

--- foob/controllers/main.py
+++ foob/controllers/main.py
@@ -4,13 +4,13 @@
 :license: CC0 1.0 Universal, see LICENSE for more details.
 """
 
-from flask import Blueprint, current_app, request, redirect, render_template, jsonify
+from flask import Blueprint, render_template
 
 
 main = Blueprint('main', __name__)
 
 
-@main.route('/', methods=['GET',])
+@main.route('/', methods=['GET', ])
 def indexPage():
     templateContext = {}
     return render_template('index.jinja', **templateContext)
